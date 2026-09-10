116f73d652098ee0d2c91067a70f2d59e15d4ae1
tyha <thienty.ha@estuary.solutions>
2026-09-11 00:44:19 +0700

    fix: update PM2 configuration to include homepage setting

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,11 +3,11 @@ module.exports = {
     {
       name: "techpulse-frontend",
       script: "serve",
-      args: "-s dist -l 5174",
       env: {
         PM2_SERVE_PATH: "./dist",
         PM2_SERVE_PORT: 5174,
         PM2_SERVE_SPA: "true",
+        PM2_SERVE_HOMEPAGE: "./dist/index.html",
       },
     },
   ],
