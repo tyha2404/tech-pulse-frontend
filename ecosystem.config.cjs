fc83bb69fe5914baf3528e816f6706b9e7e44a2c
tyha <thienty.ha@estuary.solutions>
2026-09-11 00:47:07 +0700

    fix: remove homepage setting from PM2 configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,7 +7,6 @@ module.exports = {
         PM2_SERVE_PATH: "./dist",
         PM2_SERVE_PORT: 5174,
         PM2_SERVE_SPA: "true",
-        PM2_SERVE_HOMEPAGE: "./dist/index.html",
       },
     },
   ],
